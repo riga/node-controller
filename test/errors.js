5250616073669dea8df77606f18002626278a9ea
marcel <rieger@physik.rwth-aachen.de>
2014-03-07 15:56:17 +0100

    Remove return values, add domains and emitters for error handling, v0.2.4.

---
+++ test/errors.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var app = express();
+
+var Controller = require("../lib/controller.js");
+
+var NodeCtrl = Controller._extend({
+
+  init: function(parent, config) {
+    this._super(parent, config);
+  },
+
+  _index_: function(req, res) {
+    res.send("Hello Controller!");
+  },
+
+  _simplefail_: function(req, res) {
+    console.log("simple fail triggered");
+    throw 404;
+  },
+
+  _complexfail_: function(req, res) {
+    console.log("complex fail triggered");
+    setTimeout(function() {
+      throw 404;
+    }, 1000);
+  },
+
+  _emitterfail_: function(req, res) {
+    var self = this;
+
+    console.log("emitter fail triggered");
+    setTimeout(function() {
+      self.emit("error", 403);
+    }, 1000);
+  }
+});
+
+var ctrl = new NodeCtrl();
+app.use(ctrl.middleware());
+// or
+// ctrl.bind(app);
+
+app.listen(3000);
